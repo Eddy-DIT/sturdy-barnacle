d0e31adeb6f56a0eb4470d6675508c4136874545
IDRISSA SYALLA <welleidy@gmail.com>
2024-07-03 19:22:23 +0000

    Méthode 3 pour chebyshev_distance

--- chebyshev_distance.py
+++ chebyshev_distance.py
@@ -7,3 +7,7 @@ def chebyshev_distance(point1, point2):
 
 def chebyshev_distance(point1, point2):
     return max(map(lambda x: abs(x[0] - x[1]), zip(point1, point2)))
+
+
+def chebyshev_distance(point1, point2):
+    return max([abs(a - b) for a, b in zip(point1, point2)])
